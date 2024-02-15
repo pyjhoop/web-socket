e5d1d6b79c35d92dabaeb701c9c97b03930c9fc1
pyjhoop <pyjhoop@naver.com>
2024-02-15 10:04:03 +0900

    simple WebSocket

---
+++ websocket/src/main/java/com/example/websocket/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","http://localhost:8081","http://127.0.0.1:3000","http://127.0.0.1:8081") // 허용할 Origin(도메인)을 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 설정
+                .maxAge(3000);
+    }
+
+
+}
---
+++ websocket/src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.example.websocket.config;
+
+import com.example.websocket.handler.TutorialHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(tutorialHandler(), "/tutorial")
+                .setAllowedOrigins("*");
+    }
+
+    @Bean
+    WebSocketHandler tutorialHandler() {
+        return new TutorialHandler();
+    }
+}
